4c621e68e95e979c8a32941b19edb9791c9737ff
Murilo Barbosa <muriloab96@gmail.com>
2024-02-12 11:00:49 -0300

    Added two methods to the HomeService. GetAllHomes and AddNewHome

---
+++ HomesFullStack.Server/Services/HomeService.cs
@@ -0,0 +1,49 @@
+﻿using HomesFullStack.Server.Data;
+using HomesFullStack.Server.Models;
+using HomesFullStack.Server.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomesFullStack.Server.Services
+{
+    public class HomeService : IHomeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddNewHomeAsync(Home home)
+        {
+            try
+            {
+                _context.Add(home);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Home>> GetAllHomesAsync()
+        {
+            List<Home> result = new List<Home>();
+
+            try
+            {
+
+                result = await _context.Homes.ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ HomesFullStack.Server/Services/Interfaces/IHomeService.cs
@@ -0,0 +1,13 @@
+﻿using HomesFullStack.Server.Models;
+
+namespace HomesFullStack.Server.Services.Interfaces
+{
+    public interface IHomeService
+    {
+
+        public Task<List<Home>> GetAllHomesAsync();
+        public Task AddNewHomeAsync (Home home);
+        
+
+    }
+}
